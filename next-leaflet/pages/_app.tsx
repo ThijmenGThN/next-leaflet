5cd726883e125827ac592010a04d29be03632938
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-01-03 02:36:56 +0100

    revert

--- next-leaflet/pages/_app.tsx
+++ next-leaflet/pages/_app.tsx
@@ -8,7 +8,7 @@ import '@/styles/globals.css'
 
 const { publicRuntimeConfig: config } = getConfig()
 
-export default function App({ Component, pageProps }: { Component: any, pageProps: AppProps }) {
+export default function App({ Component, pageProps }: any) {
 
   pageProps.API = new Directus(config.CORS_ENDPOINT ? config.CORS_ENDPOINT : 'http://localhost:8055')
 
