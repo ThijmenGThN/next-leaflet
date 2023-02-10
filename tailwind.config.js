a4eaf5509e1dea535281286a528958f639679771
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-02-10 16:45:31 +0100

    🎨 Ability to write custom color vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,13 @@ module.exports = {
   content: [
     "./pages/**/*.{ts,tsx}",
     "./source/components/**/*.{ts,tsx}"
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#11999e",
+        secondary: "#0c7275",
+      }
+    }
+  }
 }
