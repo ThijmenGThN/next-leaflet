434ddeeea1f3a22df77142af0497e2522d780d3c
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-06-04 22:28:47 +0200

    ♻️ Small refactoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import '@/styles/globals.css'
 export const metadata = {
     title: 'next-leaflet',
     description: 'An optimized tech stack for efficiency.',
-    icons: {
-        icon: '/favicon.ico'
-    }
+    icons: { icon: '/favicon.ico' }
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
