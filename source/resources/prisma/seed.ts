ef0c29eb539a4f6757ca3ef2bf22193ad36e141a
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-06-09 15:38:11 +0200

    🔒️ Partial next-auth

---
+++ source/resources/prisma/seed.ts
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt")
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            email: 'admin@leaflet.app',
+            password: await bcrypt.hash('alpine', 10)
+        }
+    })
+}
+
+main()
+    .then(async () => await prisma.$disconnect())
+    .catch(async (e) => await prisma.$disconnect())
