dae0009102ebae232f52420d77252671d21e8900
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-08-06 02:26:32 +0200

    🚑 Hotfix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -16,20 +16,28 @@ export const metadata: Metadata = {
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default async function Layout({ children, params: { locale } }: { children: React.ReactNode, params: any }) {
-    let locales
-
-    try {
-        locales = (await import(`../../src/locales/${locale}.json`)).default
-        if (locale !== (useLocale())) throw new Error('Attempt to use non-existent locale.')
-    }
+export default async function Logic({ children, params: { locale } }: { children: React.ReactNode, params: any }) {
+    let messages
+    try { messages = (await import(`../../src/locales/${locale}.json`)).default }
     catch (_) { notFound() }
+
+    return (
+        <Layout locale={locale} messages={messages}>
+            {children}
+        </Layout>
+    )
+
+}
+
+function Layout({ locale, messages, children }: { locale: string, messages: any, children: React.ReactNode }) {
+
+    if (locale !== (useLocale())) notFound()
 
     return (
         <html lang="en" className="h-full">
             <body className={inter.className + ' h-full'}>
                 <Session>
-                    <NextIntlClientProvider locale={locale} messages={locales}>
+                    <NextIntlClientProvider locale={locale} messages={messages}>
                         {children}
                     </NextIntlClientProvider>
                 </Session>
