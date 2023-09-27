a3cfc07c9a59252705495dde601d203bb3d4f2ad
ThijmenGThN <mail@thijmenheuvelink.nl>
2023-09-27 14:31:13 +0000

    💫 next-intl setup

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
+import { notFound } from 'next/navigation'
 
 import Session from './Session'
+import { locales } from '../../middleware'
 
 import '@/styles/globals.css'
 
@@ -13,10 +15,12 @@ export const metadata: Metadata = {
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children, params: { locale } }: { children: React.ReactNode, params: { locale: string } }) {
+
+    locales.some(cur => cur === locale) ?? notFound()
 
     return (
-        <html lang="en" className="h-full">
+        <html lang={locale} className="h-full">
             <body className={inter.className + ' h-full'}>
                 <Session>
                     {children}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 
-module.exports = {
+const withNextIntl = require('next-intl/plugin')('./src/helpers/locales.ts')
+
+module.exports = withNextIntl({
     images: {
         domains: ['www.gravatar.com']
     }
-}
+})
