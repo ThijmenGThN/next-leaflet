2adbdd96a3182671a1a78bb28dbe885dc375ee14
ThijmenGThN <mail@thijmenheuvelink.nl>
2023-11-23 23:17:37 +0000

    🚨 Lint fix

--- src/helpers/encoding.ts
+++ src/helpers/encoding.ts
@@ -1,5 +1,7 @@
 
-export default {
+const utils = {
     toBase64: (plainText: string) => Buffer.from(plainText, 'utf8').toString('base64'),
     fromBase64: (encodedText: string) => Buffer.from(encodedText.replaceAll('%3D', '='), 'base64').toString('utf8')
 }
+
+export default utils
