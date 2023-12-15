538b7302381eeffa7d7572e5126496d36a8f34f8
ThijmenGThN <mail@thijmenheuvelink.nl>
2023-12-15 17:19:48 +0000

    ♻️ Refactored

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -2,8 +2,18 @@
 export default function Page() {
 
     return (
-        <>
+        <div className="flex flex-col gap-y-4 items-center my-16">
+
             Dashboard
-        </>
+
+            <p className="text-3xl font-semibold">
+                next-leaflet
+            </p>
+
+            <p className="text-sm">
+                This route has been protected via the template file.
+            </p>
+
+        </div>
     )
 }
