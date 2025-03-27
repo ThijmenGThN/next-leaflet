0f675ca810521a77739abf0a9e3023975c6c9430
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-03-27 17:16:40 +0100

    Workaround payload during build database error

--- src/functions/connector.ts
+++ src/functions/connector.ts
@@ -5,5 +5,6 @@ import { BasePayload, getPayload as init } from 'payload'
 let payload: BasePayload | null = null
 
 export async function getPayload() {
+    if (process.env.DOCKER == 'building') return {} as BasePayload
     return payload ?? (payload = await init({ config }))
 }
