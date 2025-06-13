2baa06a4d18793a63decade190a11c060abde427
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-06-13 12:29:43 +0200

    Added base migrations

--- src/backend/migrations/index.ts
+++ src/backend/migrations/index.ts
@@ -1,4 +1,9 @@
+import * as migration_20250613_102937 from './20250613_102937';
 
 export const migrations = [
-
+  {
+    up: migration_20250613_102937.up,
+    down: migration_20250613_102937.down,
+    name: '20250613_102937'
+  },
 ];
