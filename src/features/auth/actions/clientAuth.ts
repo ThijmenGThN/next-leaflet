90c013e2ecd1148e272768c5ec80b95ac8c36a1d
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-09-21 02:01:35 +0200

    Amend

--- src/features/auth/actions/clientAuth.ts
+++ src/features/auth/actions/clientAuth.ts
@@ -1,17 +1,14 @@
 "use server";
 
-import { headers as nextHeaders } from "next/headers";
 import { getPayload } from "@/lib/payload";
 
 export async function loginUser(data: { email: string; password: string }) {
 	const payload = await getPayload();
-	const headers = await nextHeaders();
 
 	try {
 		const result = await payload.login({
 			collection: "users",
 			data,
-			headers,
 		});
 
 		return {
