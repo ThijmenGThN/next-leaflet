7ab72fbece88d15ca98e4e293bc4dfc3077b6f77
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-12-02 21:09:59 +0100

    Revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
-import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 
 import ConvexClientProvider from "@/components/ConvexClientProvider"
 import { ThemeSync } from "@/components/ThemeSync"
+import { Toaster } from "@/components/ui/sonner"
+
+import type { Metadata } from "next"
 
 import "@/styles/globals.css"
-import { Toaster } from "@/components/ui/sonner"
 
 export const metadata: Metadata = {
 	title: "next-leaflet",
--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,11 +1,19 @@
 "use client"
 
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs"
 import { ConvexReactClient } from "convex/react"
-import type { ReactNode } from "react"
+import { ReactNode } from "react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
-export default function ConvexClientProvider({ children }: { children: ReactNode }) {
-	return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  )
 }
