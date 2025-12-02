7c80771ec888020105221a2d89a66735fcac41f2
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-12-02 23:13:23 +0100

    Domain test

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -4,7 +4,7 @@ import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs"
 import { ConvexReactClient } from "convex/react"
 import { ReactNode } from "react"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const convex = new ConvexReactClient("https://cvx-i-nl.heuve.link")
 
 export default function ConvexClientProvider({
   children,
