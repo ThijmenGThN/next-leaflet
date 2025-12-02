c9bbada65bc27194c8874e4c0cf15249c57c1172
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-12-02 17:51:38 +0100

    React client provider

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs"
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react"
 import type { ReactNode } from "react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function ConvexClientProvider({ children }: { children: ReactNode }) {
-	return <ConvexAuthNextjsProvider client={convex}>{children}</ConvexAuthNextjsProvider>
+	return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>
 }
